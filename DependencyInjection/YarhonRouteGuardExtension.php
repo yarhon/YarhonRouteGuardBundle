2ff27d9f142f3b70e84bb1f57e1e2868e2a09deb
Yaroslav Honcharuk <yaroslav.xs@gmail.com>
2018-11-10 19:14:02 +0200

    Reorganized services.xml into a set of files

--- DependencyInjection/YarhonRouteGuardExtension.php
+++ DependencyInjection/YarhonRouteGuardExtension.php
@@ -32,8 +32,10 @@ class YarhonRouteGuardExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
         $loader->load('cache.xml');
+        $loader->load('controller.xml');
+        $loader->load('security.xml');
+        $loader->load('twig.xml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
